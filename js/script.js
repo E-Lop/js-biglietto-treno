1016f5a74a0683279eee2cacf8714c9883834106
E-Lop <edmondolopez@gmail.com>
2022-06-01 14:33:21 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -10,20 +10,19 @@ L'output del prezzo finale va messo fuori in forma umana
 
 // chiedere a utente quanti km deve viaggiare
 const tripLength = parseInt(prompt('Quanti km devi percorrere in treno?'));
-console.log(tripLength);
 
 // chiedere a utente quanti anni ha
 const userAge = parseInt(prompt('Quanti anni hai?'));
-console.log(userAge);
 
 // calcolare prezzo standard del biglietto
 let ticketPrice = tripLength * 0.21;
-console.log(ticketPrice);
 
 let discountedTicketPrice;
 let messageToUser;
+
 // se utente ha meno di 18 anni applicare 20% di sconto
 // se utente ha più di 65 anni applicare 40% di sconto
+// altrimenti applicare prezzo standard
 if (userAge < 18) {
   discountedTicketPrice = ticketPrice * 0.8;
   messageToUser = `Il costo del biglietto è ${discountedTicketPrice.toFixed(
