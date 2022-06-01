f712b729d758507396033966cb7edec2b3040763
E-Lop <edmondolopez@gmail.com>
2022-06-01 14:02:03 +0200

    suddivisione logica degli step

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere 
+e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana 
+(con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+// chiedere a utente quanti km deve viaggiare
+// chiedere a utente quanti anni ha
+// calcolare prezzo standard del biglietto
+// se utente ha meno di 18 anni applicare 20% di sconto
+// se utente ha più di 65 anni applicare 40% di sconto
+// pubblicare in pagina il prezzo del biglietto arrotondato ai centesimi di euro
